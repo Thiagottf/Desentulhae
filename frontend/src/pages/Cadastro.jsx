9f8307bff77948e61c4010a3382fd029d7a68cbd
Lucas Andrey <luquinhasandrey@hotmail.com>
2025-05-28 12:10:29 -0300

    Cadastro melhorado 2

--- frontend/src/pages/Cadastro.jsx
+++ frontend/src/pages/Cadastro.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Input from '../components/Input';
-import { api } from '../services/api';
+import  api  from '../services/api';
 
 const Cadastro = () => {
   const navigate = useNavigate();
